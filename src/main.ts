3156ebad267396f3a325d5f419511dcdfd965937
Lee SH <snghyun331@gmail.com>
2024-01-16 22:51:45 +0900

    fix: swagger api경로 수정

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   // run swagger
   const config = new SwaggerConfig().initializeOptions();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/auth/swagger', app, document, {
+  SwaggerModule.setup('api/user/swagger', app, document, {
     swaggerOptions: { defaultModelsExpandDepth: -1 },
   });
 
