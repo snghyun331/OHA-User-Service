7b7343d7e1d88350dcdab88f479549b30ead4fe3
Lee SH <snghyun331@gmail.com>
2024-06-07 11:35:09 +0900

    Modify: [#42] 닉네임 디폴트 수정

---
+++ src/utils/utility.ts
@@ -0,0 +1,13 @@
+import { RandomNames } from './randomname';
+
+export const createRandomName = () => {
+  const randomIndex = Math.floor(Math.random() * RandomNames.length);
+  return RandomNames[randomIndex];
+};
+
+export const createNickName = (name: string) => {
+  const noSpaceName = name.replace(/(\s*)/g, '');
+  const randomNumber = Math.floor(Math.random() * 100) + 10;
+  const nickname = `${noSpaceName}${randomNumber}`;
+  return nickname;
+};
